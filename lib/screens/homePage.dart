4248f24dbf81e54494cce655a3e84ef4bd4e83d9
Koustav Manna <koustavmanna223@drdmtech.com>
2025-03-06 13:50:12 +0530

    made some ui changes on homePage

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -11,53 +11,50 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(MediaQuery.of(context).size.height / 6),
-        child: AppBar(
-          toolbarHeight: 80,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(20),
-                  bottomLeft: Radius.circular(20))),
-          backgroundColor: const Color.fromARGB(255, 255, 226, 211),
-          flexibleSpace: Padding(
-            padding: EdgeInsets.fromLTRB(35, 45, 0, 0),
-            child: Container(
-              // color: Colors.red,
-              child: const Text(
-                "Zapchat",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF241437),
-                ),
-              ),
-            ),
-          ),
-          bottom: Tab(
-            height: (MediaQuery.of(context).size.height / 5) - 56,
-            child: Container(
-              width: double.infinity,
-              // color: Colors.amber,
-              padding: EdgeInsets.fromLTRB(35, 30, 20, 20),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "From quick chats to deep conversations ZapChat keeps you connected",
-                    style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        color: Color(
-                          0xFF241437,
-                        ),
-                        fontSize: 18),
-                  )
-                ],
-              ),
-              // color: Colors.yellow,
-            ),
-          ),
-        ),
+      appBar: AppBar(
+        // toolbarHeight: 80,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(20),
+                bottomLeft: Radius.circular(20))),
+        backgroundColor: const Color.fromARGB(255, 255, 226, 211),
+        // flexibleSpace: Padding(
+        //   padding: EdgeInsets.fromLTRB(35, 45, 0, 0),
+        //   child: Container(
+        //     // color: Colors.red,
+        //     child: const Text(
+        //       "",
+        //       style: TextStyle(
+        //         fontSize: 40,
+        //         fontWeight: FontWeight.bold,
+        //         color: Color(0xFF241437),
+        //       ),
+        //     ),
+        //   ),
+        // ),
+        // bottom: Tab(
+        //   height: (MediaQuery.of(context).size.height / 5) - 56,
+        //   child: Container(
+        //     width: double.infinity,
+        //     // color: Colors.amber,
+        //     padding: EdgeInsets.fromLTRB(35, 30, 20, 20),
+        //     child: const Column(
+        //       crossAxisAlignment: CrossAxisAlignment.start,
+        //       children: [
+        //         Text(
+        //           "From quick chats to deep conversations ZapChat keeps you connected",
+        //           style: TextStyle(
+        //               fontWeight: FontWeight.w500,
+        //               color: Color(
+        //                 0xFF241437,
+        //               ),
+        //               fontSize: 18),
+        //         )
+        //       ],
+        //     ),
+        //     // color: Colors.yellow,
+        //   ),
+        // ),
       ),
       body: Container(
           width: double.infinity,
@@ -65,10 +62,27 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             // mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Text("Helllo"),
-              Image.asset(
-                'assets/images/homepage.png',
-                height: 400,
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Zapchat",
+                style: TextStyle(
+                  // backgroundColor: Colors.cyan,
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF241437),
+                ),
+              ),
+              Container(
+                // color: Colors.amber,
+                child: Image.asset(
+                  'assets/images/homepage.png',
+                  height: 350,
+                ),
+              ),
+              const SizedBox(
+                height: 40,
               ),
               const Text(
                 "Welcome to Zapchat.!",
